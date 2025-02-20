0eda8ea7c9a51a2caf07dd532c4b7db5d97b4479
Akash <thakare.akash007@gmail.com>
2025-02-20 14:22:13 +0530

    add SubNav component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import SubNav from "./components/SubNav";
 
 function App() {
   return (
     <>
     <Navbar/>
+    <SubNav/>
      <Home/>
     </>
   );
---
+++ src/components/SubNav.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+const SubNav = () => {
+  const [tableNumber, setTableNumber] = useState(4);
+  const [bill, setBill] = useState([]);
+
+  return (
+    <div >
+      <nav className="bg-gray-100  shadow h-12 flex items-center  space-x-4 px-4">
+        <button className="bg-white hover:bg-gray-300 px-4 py-2 rounded-lg flex items-center cursor-pointer">
+          <i className="fa-regular fa-file mr-2"></i> Bill
+        </button>
+        <div className="flex items-center space-x-1">
+        <i class="fa-solid fa-mug-saucer"></i>
+          <span>{tableNumber}</span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default SubNav;
